626e83177ee837f0d6edac80c7b5d61840329a42
kudari00 <yehong@brandeis.edu>
2019-05-08 16:40:24 -0400

    helper

--- helper.py
+++ helper.py
@@ -4,45 +4,44 @@ def hashtag_filter(query):
     """
     title #field: display the content of a field
     :param query
-    :return: str - the field name
+    :return: str - the field name, str - otherwords
     """
-    raw = re.findall("#.+", query)
-    return raw[0][1:]
+    field_name = re.findall("#.+", query)
+    other_words = re.sub("#.+", "", query)
+    return field_name[0][1:], other_words
 
 def phrase_filter(query):
     """
     “”: phrase search
     :param query
-    :return: list of str - phrases
+    :return: list of str - phrases, str - otherwords
     """
-    raw=re.findall("\".*?\"", query)
-    phrase_list=[]
-    for phrase in raw:
-        if phrase=="\"\"":continue
-        phrase_list.append(phrase[1:-1])
-
-    return phrase_list
+    phrase_list = [r[1: -1] for r in re.findall('\\".*?\\"', query) if r != '""']
+    other_words = re.sub('\\".*?\\"', "", query)
+    return phrase_list, other_words
 
 def difference_filter(query):
     """
     -word :find out words that should be eliminated
     :param query
-    :return: str - word
+    :return: str - word, str - otherwords
     """
 
     raw = re.findall("-\w+", query)
-    return raw[0][1:]
+    other_words = re.sub("-\w+", "", query)
+    return raw[0][1:], other_words
 
 def conjunction_filter(query):
     """
     +word: find out words that must be included in the search result
     :param query
-    :return: str - word
+    :return: str - word, str - otherwords
     """
     raw = re.findall("\+\w+", query)
-    return raw[0][1:]
+    other_words = re.sub("\+\w+", "", query)
+    return raw[0][1:], other_words
 
-query="this\"haha\"a\"this\" gaga \"\""
+query="this\"haha\" a\" this\" gaga \"\""
 print(phrase_filter(query))
 query="book title #plot summary"
 print(hashtag_filter(query))
