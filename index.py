07ea7c4e23582f4fe297f2e85c8df74c1c030312
chauncy <chauncyf@qq.com>
2019-05-10 19:40:39 -0400

    rename

--- index.py
+++ index.py
@@ -194,7 +194,7 @@ def build_index():
     # Open the json film corpus
     books = {}
     mid = 1
-    with open('./sparknotes/shelve/sparknotes_book_detail.jl',encoding='UTF-8') as data_file:
+    with open('./sparknotes/shelve/merged_sparknote.jl', encoding='UTF-8') as data_file:
         # load books from json file into dictionary
 
         for line in data_file:
@@ -234,7 +234,7 @@ def build_summary_index():
 
     books = {}
     mid = 1
-    with open('./sparknotes/shelve/sparknotes_book_detail.jl',encoding='UTF-8') as data_file:
+    with open('./sparknotes/shelve/merged_sparknote.jl', encoding='UTF-8') as data_file:
         for line in data_file:
             books[str(mid)] = json.loads(line)
             books[str(mid)] = makeup_fields(books[str(mid)])
@@ -267,7 +267,7 @@ def build_summary_sentence_index():
 
     books = {}
     mid = 1
-    with open('./sparknotes/shelve/sparknotes_book_detail.jl', encoding='UTF-8') as data_file:
+    with open('./sparknotes/shelve/merged_sparknote.jl', encoding='UTF-8') as data_file:
 
         for line in data_file:
             books[str(mid)] = json.loads(line)
@@ -301,7 +301,7 @@ def build_main_ideas_index():
 
     books = {}
     mid = 1
-    with open('./sparknotes/shelve/sparknotes_book_detail.jl',encoding='UTF-8') as data_file:
+    with open('./sparknotes/shelve/merged_sparknote.jl', encoding='UTF-8') as data_file:
 
         for line in data_file:
             books[str(mid)] = json.loads(line)
@@ -342,7 +342,7 @@ def build_character_index():
     # Open the json film corpus
     books = {}
     mid = 1
-    with open('./sparknotes/shelve/sparknotes_book_detail.jl',encoding='UTF-8') as data_file:
+    with open('./sparknotes/shelve/merged_sparknote.jl', encoding='UTF-8') as data_file:
         # load books from json file into dictionary
 
         for line in data_file:
@@ -383,7 +383,7 @@ def build_quotes_index():
 
     books = {}
     mid = 1
-    with open('./sparknotes/shelve/sparknotes_book_detail.jl',encoding='UTF-8') as data_file:
+    with open('./sparknotes/shelve/merged_sparknote.jl', encoding='UTF-8') as data_file:
 
         for line in data_file:
             books[str(mid)] = json.loads(line)
@@ -421,7 +421,7 @@ def build_author_index():
 
     books = {}
     mid = 1
-    with open('./sparknotes/shelve/sparknotes_book_detail.jl',encoding='UTF-8') as data_file:
+    with open('./sparknotes/shelve/merged_sparknote.jl', encoding='UTF-8') as data_file:
 
         for line in data_file:
             books[str(mid)] = json.loads(line)
@@ -454,7 +454,7 @@ def build_title_index():
 
     books = {}
     mid = 1
-    with open('./sparknotes/shelve/sparknotes_book_detail.jl',encoding='UTF-8') as data_file:
+    with open('./sparknotes/shelve/merged_sparknote.jl', encoding='UTF-8') as data_file:
 
         for line in data_file:
             books[str(mid)] = json.loads(line)
