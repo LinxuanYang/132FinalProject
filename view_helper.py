8cf1f45831c25dec31dd489c8afc55683e7050ba
gggllm <515815530@qq.com>
2019-05-11 15:11:43 -0400

    add more like this router

--- view_helper.py
+++ view_helper.py
@@ -9,6 +9,7 @@ def render_result(params):
     mes = params.get('message', [])
     results = params.get('result_list', [])
     query = params.get('query')
+    book_id = params.get('book_id')  # this is for similar book function, I use this for pagination
     # print(list(map(lambda x:x['title'],results)))
     return render_template('search_result.html.jinja2', show={query}, page_number=page_number, page_size=page_size,
                            res_num=res_num, results=results, query_id=query_id, mes=mes)
