79d496637f2297edb434635e9b86d432b9bd0745
gggllm <515815530@qq.com>
2019-05-12 22:21:01 -0400

    add connection in booster_helper

--- booster_helper.py
+++ booster_helper.py
@@ -1,10 +1,19 @@
 from collections import defaultdict
+
+from elasticsearch import Elasticsearch
+
 import field_booster
 import math
 import nltk
 from data_base import *
-from elasticsearch_dsl import Search
+from elasticsearch_dsl import Search, connections
 from query_helper import fields_list, index_name, parse_result
+
+connections.create_connection(hosts=['127.0.0.1'])
+
+# Create elasticsearch object
+es = Elasticsearch()
+
 
 def fieldsearch_scores(query):
     """
@@ -27,6 +36,7 @@ def fieldsearch_scores(query):
         scores.append(sum / float(total) if total != 0 else 0)
     return length_normalization(scores)
 
+
 def userdata_scores(query, behave_data):
     """
     analyze user behaviour data
@@ -40,13 +50,15 @@ def userdata_scores(query, behave_data):
         dict[key_set[0]] = behave_data[key] if key_set[1] == 'click' else dict[key_set[0]]
     for key in behave_data:
         key_set = key.split(" ")
-        dict[key_set[0]] = (0.5 * dict[key_set[0]] * behave_data[key])/float(1500) if key_set[1] == 'hover' else dict[key_set[0]]
+        dict[key_set[0]] = (0.5 * dict[key_set[0]] * behave_data[key]) / float(1500) if key_set[1] == 'hover' else dict[
+            key_set[0]]
     for field in fields_list[0:11]:
         if field in dict:
             scores.append(dict[field])
         else:
             scores.append(0)
     return length_normalization(scores)
+
 
 def length_normalization(scores):
     total = 0
@@ -55,6 +67,7 @@ def length_normalization(scores):
     for i in range(0, len(scores)):
         scores[i] = scores[i] / math.sqrt(total) if total != 0 else 0
     return scores
+
 
 def balance_scores(fieldsearch, userdata, co, threshold):
     """
@@ -65,6 +78,7 @@ def balance_scores(fieldsearch, userdata, co, threshold):
     """
     result = list(map(lambda x, y: round(co * x + (1 - co) * y + 0.5 - threshold), fieldsearch, userdata))
     return result
+
 
 def extract_features(query):
     """
@@ -93,6 +107,7 @@ def extract_features(query):
     feature_set.append(none_num)
     return feature_set
 
+
 def load_from_database():
     """
     load data from database
@@ -104,8 +119,10 @@ def load_from_database():
     for q in queries:
         result = defaultdict(int)
         id = q.id
-        clicks = Click.select(fn.Count('*').alias('count'), Click.field.alias('field')).where(Click.query_id==id).group_by(Click.field)
-        hovers = Click.select(Click.field.alias('field'), fn.Sum(Hover.duration).alias('duration')).join(Query).join(Hover).where((Click.query_id==id) & (Hover.query_id==id)).group_by(Click.field)
+        clicks = Click.select(fn.Count('*').alias('count'), Click.field.alias('field')).where(
+            Click.query_id == id).group_by(Click.field)
+        hovers = Click.select(Click.field.alias('field'), fn.Sum(Hover.duration).alias('duration')).join(Query).join(
+            Hover).where((Click.query_id == id) & (Hover.query_id == id)).group_by(Click.field)
         for c in clicks:
             result[c.field + ' click'] = c.count
         for h in hovers:
@@ -114,6 +131,7 @@ def load_from_database():
         results[q.query] = result
 
     return results
+
 
 def preprocess_training_data():
     """
@@ -140,8 +158,10 @@ def preprocess_training_data():
         Y.append(balance_scores(fieldsearch_scores(query), userdata_scores(query, data[query]), 0.4, 0.2))
     return X, Y
 
+
 def get_classifier():
     return classifier
+
 
 X, Y = preprocess_training_data()
 classifier = field_booster.FieldBooster(X, Y)
